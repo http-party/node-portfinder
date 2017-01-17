aa6d96e5ce536165f2332170cb4557a612b36d95
Julien CROUZET <contact@juliencrouzet.fr>
2017-01-17 21:08:05 +0100

    TS definition fixes

--- lib/portfinder.d.ts
+++ lib/portfinder.d.ts
@@ -4,26 +4,26 @@
  * (C) 2011, Charlie Robbins
  */
 
-declare type PortfinderHandler = (err: Error, port: number) => void;
+type PortfinderCallback = (err: Error, port: number) => void;
 
-declare interface PortFinderOptions{
+interface PortFinderOptions{
   /**
    * Host to find available port on.
    */
-  host: string;
+  host?: string;
   /**
    * Minimum port (takes precedence over `basePort`).
    */
-  port: number;
+  port?: number;
 }
 
 /**
  * The lowest port to begin any port search from.
  */
-export declare let basePort: number;
+export let basePort: number;
 
 /**
  * Responds with a unbound port on the current machine.
  */
-export declare function getPort(options: PortFinderOptions, callback: PortfinderHandler): void;
-export declare function getPort(callback: PortfinderHandler): void;
+export function getPort(callback: PortfinderCallback): void;
+export function getPort(options: PortFinderOptions, callback: PortfinderCallback): void;
