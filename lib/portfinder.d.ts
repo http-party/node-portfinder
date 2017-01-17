cfe29735212092c8794b7f1ea1d45e8c52c7f230
Julien CROUZET <contact@juliencrouzet.fr>
2017-01-17 19:49:10 +0100

    Add options argument

--- lib/portfinder.d.ts
+++ lib/portfinder.d.ts
@@ -6,6 +6,17 @@
 
 declare type PortfinderHandler = (err: Error, port: number) => void;
 
+declare interface PortFinderOptions{
+  /**
+   * Host to find available port on.
+   */
+  host: string;
+  /**
+   * Minimum port (takes precedence over `basePort`).
+   */
+  port: number;
+}
+
 /**
  * The lowest port to begin any port search from.
  */
@@ -14,4 +25,5 @@ export declare let basePort: number;
 /**
  * Responds with a unbound port on the current machine.
  */
+export declare function getPort(options: PortFinderOptions, callback: PortfinderHandler): void;
 export declare function getPort(callback: PortfinderHandler): void;
