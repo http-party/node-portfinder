874c290a204a60425f7beb724a39f787b527b366
Erik Trom <erik.trom@gmail.com>
2017-03-08 14:34:54 -0800

    wip - dont try to catch errors; make tests slightly more manageable

--- test/port-finder-test.js
+++ test/port-finder-test.js
@@ -18,12 +18,74 @@ var debugVows = debug('portfinder:testVows');
 portfinder.basePort = 32768;
 
 var servers = [];
+var waitTime = 3000; // wait for cleanup of bound hosts
 
-vows.describe('portfinder').addBatch({
+vows.describe('portfinder')
+.addBatch({
+  "When using portfinder module": {
+    "the getPort() method": {
+      topic: function () {
+        portfinder.getPort(this.callback);
+      },
+      "should respond with the first free port (32768)": function (err, port) {
+        if (err) { debugVows(err); }
+        assert.isTrue(!err);
+        assert.equal(port, 32768);
+      }
+    }
+  }
+}).addBatch({
+  "When using portfinder module": {
+    "the getPortPromise() method": {
+      topic: function () {
+        var vow = this;
+
+        portfinder.getPortPromise()
+          .then(function (port) {
+            vow.callback(null, port);
+          })
+          .catch(function (err) {
+            vow.callback(err, null);
+          });
+      },
+      "should respond with a promise of first free port (32768) if Promise are available": function (err, port) {
+        if (typeof Promise !== 'function') {
+          assert.isTrue(!!err);
+          assert.equal(
+            err.message,
+            'Native promise support is not available in this version of node.' +
+            'Please install a polyfill and assign Promise to global.Promise before calling this method'
+          );
+          return;
+        }
+        if (err) { debugVows(err); }
+        assert.isTrue(!err);
+        assert.equal(port, 32768);
+      }
+    }
+  }
+})
+.addBatch({
+  "When using portfinder module": {
+    "the getPorts() method with an argument of 3": {
+      topic: function () {
+        portfinder.getPorts(3, this.callback);
+      },
+      "should respond with the first three available ports (32768, 32769, 32770)": function (err, ports) {
+        if (err) { debugVows(err); }
+        assert.isTrue(!err);
+        assert.deepEqual(ports, [32768, 32769, 32770]);
+      }
+    }
+  }
+})
+.addBatch({
   "When using portfinder module": {
     "with 5 existing servers": {
       topic: function () {
-        testHelper(servers, this.callback);
+        setTimeout(function() {
+          testHelper(servers, this.callback);
+        }.bind(this), waitTime);
       },
       "the getPort() method": {
         topic: function () {
@@ -39,75 +101,27 @@ vows.describe('portfinder').addBatch({
         topic: function () {
           setTimeout(function() {
             portfinder.getPort({ host: 'localhost' }, this.callback);
-          }.bind(this), 3000); // wait for cleanup of bound hosts
+          }.bind(this), waitTime);
         },
         "should respond with the first free port (32773)": function (err, port) {
           if (err) { debugVows(err); }
           assert.isTrue(!err);
           assert.equal(port, 32773);
         }
-      }
-    }
-  }
-}).addBatch({
-  "When using portfinder module": {
-    "with no existing servers": {
-      topic: function () {
-        servers.forEach(function (server) {
-          server.close();
-        });
-
-        return null;
       },
-      "the getPort() method": {
+      "the getPorts() method with an argument of 3": {
         topic: function () {
-          portfinder.getPort(this.callback);
+          setTimeout(function() {
+            portfinder.getPorts(3, this.callback);
+          }.bind(this), waitTime*2);
         },
-        "should respond with the first free port (32768)": function (err, port) {
+        "should respond with the first three available ports (32773, 32774, 32775)": function (err, ports) {
           if (err) { debugVows(err); }
           assert.isTrue(!err);
-          assert.equal(port, 32768);
+          assert.deepEqual(ports, [32773, 32774, 32775]);
         }
       }
     }
   }
-}).addBatch({
-  "When using portfinder module": {
-    "with no existing servers": {
-      topic: function () {
-        servers.forEach(function (server) {
-          server.close();
-        });
-
-        return null;
-      },
-      "the getPortPromise() method": {
-        topic: function () {
-          var vow = this;
-
-          portfinder.getPortPromise()
-            .then(function (port) {
-              vow.callback(null, port);
-            })
-            .catch(function (err) {
-              vow.callback(err, null);
-            });
-        },
-        "should respond with a promise of first free port (32768) if Promise are available": function (err, port) {
-          if (typeof Promise !== 'function') {
-            assert.isTrue(!!err);
-            assert.equal(
-              err.message,
-              'Native promise support is not available in this version of node.' +
-              'Please install a polyfill and assign Promise to global.Promise before calling this method'
-            );
-            return;
-          }
-          if (err) { debugVows(err); }
-          assert.isTrue(!err);
-          assert.equal(port, 32768);
-        }
-      },
-    }
-  }
-}).export(module);
+})
+.export(module);
