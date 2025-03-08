9dde634826af27c2f3ae7641961e3cba9bef1a6d
Matthew Peveler <matt.peveler@gmail.com>
2025-03-07 20:44:37 -0700

    Replace var with const/let
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- test/getPort.js
+++ test/getPort.js
@@ -1,15 +1,16 @@
-var portfinder = require('../lib/portfinder');
+const portfinder = require('../lib/portfinder');
 
-if(process.argv[2]) {
-  var port = process.argv[2];
-  if ( isNaN(port) ) {
+if (process.argv[2]) {
+  const port = process.argv[2];
+  if (isNaN(port)) {
     console.log('passed value is not a port');
   } else {
     portfinder.basePort = port;
   }
 }
+
 portfinder.getPort(function(err, port) {
-  if(err) {
+  if (err) {
     console.error(err);
   } else {
     console.log(port);
